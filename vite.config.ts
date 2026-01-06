c12a7844af27373c4c1e1418b48c32627763f1f1
Sanjana Devi <sanjana.devi@bonbloc.com>
2026-01-06 15:43:23 +0530

    Bug fix attempt 03

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/project-bolt-sb1-wgei9ouc/', // 👈 repo name
+  base: '/Portfolio-website/',
 })
